5cb3ac95767e12a6368bdd6d7907219151eb299e
Loha21 <uroslohinski@gmail.com>
2023-06-08 13:05:32 +0200

    e

--- projekatSIMS/UI/Dialogs/View/TourGuideView/TourGuideTourRequestsPageView.xaml.cs
+++ projekatSIMS/UI/Dialogs/View/TourGuideView/TourGuideTourRequestsPageView.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Win32;
+using projekatSIMS.Model;
 using projekatSIMS.UI.Dialogs.ViewModel.TourGuideViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,17 +31,53 @@ namespace projekatSIMS.UI.Dialogs.View.TourGuideView
             DataContext = new TourGuideTourRequestsPageModel();
         }
 
-        
+
+
+        private ImageSource _imageSource;
+        public ImageSource ImageSource
+        {
+            get { return _imageSource; }
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openDialog = new OpenFileDialog();
             openDialog.Filter = "Image files|*.bmp;*.jpg;*.png";
             openDialog.FilterIndex = 1;
-            if(openDialog.ShowDialog()==true)
+
+            string destinationFolder = "C:\\Users\\Korisnik\\OneDrive - Univerzitet u Novom Sadu\\Desktop\\simshci\\projekatSIMSHCI\\projekatSIMS\\Resources\\Images\\";
+
+            if (openDialog.ShowDialog() == true)
             {
-                //imagePicture.Source = new BitmapImage(new Uri(openDialog.FileName));
+                try
+                {
+                    string imagePath = openDialog.FileName;
+                    string fileName = System.IO.Path.GetFileName(imagePath);
+                    string destinationPath = System.IO.Path.Combine(destinationFolder, fileName);
+
+                    // Copy the image file to the destination folder
+                    File.Copy(imagePath, destinationPath, true);
+                    
+                    MessageBox.Show("Image saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to save image: {ex.Message}");
+                }
             }
         }
+
     }
 }
